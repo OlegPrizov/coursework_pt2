87f707fdb1399ddef8e5d574f821546059a9d81c
Олег Призов <oleg12345prisov@gmail.com>
2025-06-03 00:06:25 +0300

    V1.0

---
+++ src/main/java/Scheduler.java
@@ -0,0 +1,10 @@
+/**
+ * Interface for scheduling tasks to be executed on different threads.
+ */
+public interface Scheduler {
+    /**
+     * Schedules a task for execution.
+     * @param task The task to be executed
+     */
+    void execute(Runnable task);
+}
